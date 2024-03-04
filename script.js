6fc45446f4ff04eaf6a5c199393efb16114df8e6
Pirabaa Segar <61627407+pirabaasegar@users.noreply.github.com>
2024-03-04 15:08:45 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -17,19 +17,29 @@ function getWeather() {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            const temperature = data.main.temp;
+            const temperature = data.main.temp.toFixed();
             const description = data.weather[0].description;
-            const feelsLike = data.main.feels_like;
-            const precipitation = data.rain ? data.rain['1h'] : 'N/A';
+            const feelsLike = data.main.feels_like.toFixed();
+            const precipitation = data.rain ? (typeof data.rain['1h'] === 'number' ? data.rain['1h'] + '%' : 'N/A') : 'N/A';
             const visibility = data.visibility / 1000;
             const humidity = data.main.humidity;
             const city = data.name;
-            const country = data.sys.country;
+            const icon = data.weather[0].icon;
+            const minTemp = data.main.temp_min.toFixed();
+            const maxTemp = data.main.temp_max.toFixed();
+            const updatedTime = new Date(data.dt * 1000).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
 
             const displayText = `
                 <div class="weather-info_header">
-                    <h2>${city}, ${country}</h2>
-                    <p>${temperature}°C | ${description}</p>
+                    <div class="weather-info_header-left">
+                        <h2>${city}</h2>
+                        <p>${temperature}°</p>
+                    </div>
+                    <div class="weather-info_header-right">
+                        <img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather icon" draggable="false">
+                        <p>${description}</p>
+                        <p>H:${maxTemp}° L:${minTemp}°</p>
+                    </div>
                 </div>
                 <div class="weather-info_info">
                     <div class="weather-info_info-item">
@@ -38,7 +48,7 @@ function getWeather() {
                     </div>
                     <div class="weather-info_info-item">
                         <p>Precipitation</p>
-                        <p>${precipitation}%</p>
+                        <p>${precipitation}</p>
                     </div>
                     <div class="weather-info_info-item">
                         <p>Visibility</p>
@@ -61,3 +71,8 @@ function getWeather() {
 }
 
 document.getElementById('getWeatherButton').addEventListener('click', getWeather);
+document.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      getWeather();
+    }
+  });
